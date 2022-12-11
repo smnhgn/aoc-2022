77036e43cbab9d7fcb50fd17af32e3e2608fedb5
Simon Haugen <smn.hgn@gmail.com>
2022-12-11 18:33:06 +0100

    cleaup day8

--- src/day8/mod.rs
+++ src/day8/mod.rs
@@ -14,40 +14,37 @@ pub fn solve() -> usize {
     let mut visible_trees: HashSet<Point> = HashSet::new();
 
     for (y, line) in input.lines().enumerate() {
-        let trees = line
-            .chars()
-            .filter_map(|char| char.to_digit(10))
-            .collect::<Vec<_>>();
-
         tree_rows.push(vec![]);
 
-        for (x, tree) in trees.into_iter().enumerate() {
+        for (x, char) in line.chars().enumerate() {
             if y == 0 {
-                tree_columns.push(vec![])
+                tree_columns.push(vec![]);
             }
+
+            let tree = char.to_digit(10).unwrap();
 
             tree_rows[y].push(tree);
             tree_columns[x].push(tree);
         }
     }
 
-    for y in 0..tree_rows.len() {
-        for x in 0..tree_columns.len() {
-            let tree = tree_rows[y][x];
-            let left_max = tree_rows[y][..x].iter().max().unwrap_or(&0);
-            let right_max = tree_rows[y][x + 1..].iter().max().unwrap_or(&0);
-            let top_max = tree_columns[x][..y].iter().max().unwrap_or(&0);
-            let bottom_max = tree_columns[x][y + 1..].iter().max().unwrap_or(&0);
+    let y_max = tree_rows.len() - 1;
+    let x_max = tree_columns.len() - 1;
 
-            if y == 0
-                || x == 0
-                || y == tree_rows.len() - 1
-                || x == tree_columns.len() - 1
-                || tree > *left_max
-                || tree > *right_max
-                || tree > *top_max
-                || tree > *bottom_max
-            {
+    for y in 0..=y_max {
+        for x in 0..=x_max {
+            let tree_size = &tree_rows[y][x];
+            let left = &tree_rows[y][..x];
+            let right = &tree_rows[y][x + 1..];
+            let top = &tree_columns[x][..y];
+            let bottom = &tree_columns[x][y + 1..];
+            let min_size = [left, right, top, bottom]
+                .iter()
+                .filter_map(|side| side.iter().max())
+                .min()
+                .unwrap();
+
+            if [0, x_max].contains(&x) || [0, y_max].contains(&y) || tree_size > min_size {
                 visible_trees.insert(Point { x, y });
             }
         }
